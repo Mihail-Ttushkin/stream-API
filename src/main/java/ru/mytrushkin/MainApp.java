ef946cdfb6692cb2ba0c45e0dbea110129decd5f
Mihai <Mihail.trushkin@mail.ru>
2022-07-15 23:16:49 +0300

    111111111111111

---
+++ src/main/java/ru/mytrushkin/MainApp.java
@@ -0,0 +1,25 @@
+package ru.mytrushkin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainApp {
+    public static void main(String[] args) {
+        List<Persons> persons = new ArrayList<>(Arrays.asList(
+                new Persons("Вася", "Программист", 40),
+                new Persons("Петя", "Программист", 20),
+                new Persons("Коля", "Менеджер", 35),
+                new Persons("Жана", "Менеджер", 43),
+                new Persons("Люда", "Программист", 54)
+        ));
+
+        List<String> persons_str = persons.stream()
+                .filter(p -> p.position.equals("Программист"))
+                .sorted((o1, o2) -> o2.age - o1.age)
+                .map(p -> p.name).collect(Collectors.toList());
+
+        System.out.println(persons_str);
+    }
+}
---
+++ src/main/java/ru/mytrushkin/Persons.java
@@ -0,0 +1,13 @@
+package ru.mytrushkin;
+
+public class Persons {
+    public String name;
+    public String position;
+    public int age;
+
+    public Persons(String name, String position, int age) {
+        this.name = name;
+        this.position = position;
+        this.age = age;
+    }
+}
